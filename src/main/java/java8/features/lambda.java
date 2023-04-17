01ff9d41284424e2e9f2323ec593208f35bc8251
Abhishek_Udayashanka <abhishek_udayashankar@epam.com>
2023-04-17 19:05:50 +0530

    Java8 repo

---
+++ src/main/java/java8/features/lambda.java
@@ -0,0 +1,8 @@
+package java8.features;
+
+public class lambda {
+
+    public static void main(String[] args){
+        System.out.println("Sample program to demonstrate Lambda expressions");
+    }
+}
